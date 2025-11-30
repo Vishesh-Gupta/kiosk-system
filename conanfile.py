b2397c70f4297cab2ff47777fe822bb3c1635516
Vishesh Gupta <vishesh.gupta12@outlook.com>
2025-11-30 13:45:40 +0530

    Update the versions in conan and upgrade CMAKE version

--- conanfile.py
+++ conanfile.py
@@ -10,14 +10,14 @@ class KioskRecipe(ConanFile):
 
     def requirements(self):
         self.requires("libpq/15.4")
-        self.requires("libpqxx/7.8.1") 
-        self.requires("grpc/1.54.3")
+        self.requires("libpqxx/7.10.3") 
+        self.requires("grpc/1.69.0")
         self.requires("nlohmann_json/3.11.2")
         self.requires("boost/1.80.0")
         self.requires("gtest/1.13.0")
 
     def build_requirements(self):
-        self.tool_requires("cmake/3.22.6")
+        self.tool_requires("cmake/4.1.3")
         self.tool_requires("protobuf/3.21.9")
 
     def package_info(self):
